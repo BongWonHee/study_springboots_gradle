fd5d3bd982090dc9c9edaa6e142922941b8a5c4d
BongWonHee <leeban1004@gmail.com>
2023-07-27 09:38:27 +0900

    html폴더생성 및 이동 - 봉원희

--- src/main/java/com/yojulab/study_springboot/service/AuthsService.java
+++ src/main/java/com/yojulab/study_springboot/service/AuthsService.java
@@ -22,20 +22,21 @@ public class AuthsService {
     @Autowired
     Commons commonUtils;
 
+    // 권한을 같인 넣어주는것
     public Object insert(Map dataMap) {
         List authList = new ArrayList<>();
-        authList.add("ROLE_GUEST");  // default auth
-        authList.add(dataMap.get("auth"));  // choosed auth
+        authList.add("ROLE_GUEST"); // default auth
+        authList.add(dataMap.get("auth")); // choosed auth
         dataMap.put("authList", authList);
 
         String sqlMapId = "Auths.insert";
         Object result = sharedDao.insert(sqlMapId, dataMap);
         return result;
     }
-    //권한을 같인 넣어주는것
+
     public Map selectWithUSERNAME(Map dataMap) {
         String sqlMapId = "Auths.selectWithUSERNAME";
-        
+
         HashMap result = new HashMap<>();
         result.put("resultList", sharedDao.getList(sqlMapId, dataMap));
         return result;
